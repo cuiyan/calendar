20c38d2b90f3d78fe80ecbec1bfc1eab101e2348
roxylu <roxylu816@gmail.com>
2018-01-02 20:55:34 +0800

    calendar view by using juhe api

--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,42 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+// API URL
+const API_URL = "https://v.juhe.cn/calendar/day"
+// API KEY
+const API_KEY = "YOUR_KEY"
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+// Get detail for a given day
+function fetchDetail(today) {
+  var promise = new Promise(function (resolve, reject) {
+    wx.request({
+      url: API_URL,
+      data: {
+        key: API_KEY,
+        date: today
+      },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: resolve,
+      fail: reject
+    })
+  })
+  return promise
 }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+function getDetail() {
+  var tmpDate = new Date()
+  var month = tmpDate.getMonth() + 1
+  var today = tmpDate.getFullYear() + '-' + month + '-' + tmpDate.getDate()
+  return fetchDetail(today)
+    .then(function (res) {
+      console.log(res.data.result.data)
+      return res.data.result.data
+    })
+    .catch(function (err) {
+      console.log(err)
+      return {}
+    })
 }
 
 module.exports = {
-  formatTime: formatTime
+  getDetail: getDetail
 }
